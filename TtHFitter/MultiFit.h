db08f2390123c33ec8b108a471b4e0e4efc41507
Michele Pinamonti <michele.pinamonti@gmail.com>
2015-09-15 10:56:56 +0000

    fixes
    git-svn-id: svn+ssh://svn.cern.ch/reps/atlasinst/Institutes/UdineICTP/TtHFitter/trunk@392563 28d52da6-859c-4cdc-abcc-1ec3d2c02001

---
+++ TtHFitter/MultiFit.h
@@ -0,0 +1,38 @@
+#include "TtHFitter/Common.h"
+
+#include "TtHFitter/TtHFit.h"
+
+#ifndef __MultiFit__
+#define __MultiFit__
+
+class MultiFit {
+public:
+    
+    MultiFit(string name="MyMultiFit");
+    ~MultiFit();
+
+    void ReadConfigFile(string configFile,string options);
+    void AddFitFromConfig(string configFile,string options,string label,string loadSuf="");
+    void ComparePOI(string POI);
+    void CompareLimit();
+    void ComparePulls();
+
+    std::vector< TtHFit* > fFitList;
+    std::vector< string > fFitLabels;
+    std::vector< string > fFitSuffs;
+    
+    string fName;
+    string fLabel;
+    bool fShowObserved;
+    string fLimitTitle;
+    string fPOITitle;
+    
+    string fLumiLabel;
+    string fCmeLabel;
+    
+    ConfigParser *fConfig;
+    
+    string fSaveSuf;
+};
+    
+#endif
