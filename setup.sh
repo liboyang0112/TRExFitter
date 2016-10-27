4e04c202e9478560853662ff709763ade4290441
Loic Valery <loic.valery@cern.ch>
2016-10-27 22:00:39 +0200

    Test CI

--- setup.sh
+++ setup.sh
@@ -1,8 +1,6 @@
 # Setup ROOT and gcc
-export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
-alias setupATLAS='source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh'
-setupATLAS --quiet
-localSetupROOT 6.02.12-x86_64-slc6-gcc48-opt --quiet
+source /cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase/user/atlasLocalSetup.sh
+localSetupROOT 6.02.12-x86_64-slc6-gcc48-opt
 
 if [ "${ROOTSYS}" == "" ]; then
    echo -e "\033[41;1;37m Error initializing ROOT. ROOT is not set up. Please check. \033[0m"
