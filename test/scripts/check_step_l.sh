834043761b60c9484b41b203b1e74b6bb52ec608
Loic Valery <lvalery@lxplus032.cern.ch>
2016-10-30 23:23:22 +0100

    Update to avoid stupid diffs

--- test/scripts/check_step_l.sh
+++ test/scripts/check_step_l.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-diff -I "lvalery" -I "Real time" -I "mkdir" LOG_l test/logs/LOG_l
+diff -I "lvalery" -I "/builds/" -I "Real time" -I "mkdir" LOG_l test/logs/LOG_l
--- test/scripts/check_step_s.sh
+++ test/scripts/check_step_s.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-diff -I "lvalery" -I "Real time" -I "mkdir" -I "RooRealVar" LOG_s test/logs/LOG_s
+diff -I "lvalery" -I "/builds/" -I "Real time" -I "mkdir" -I "RooRealVar" LOG_s test/logs/LOG_s
