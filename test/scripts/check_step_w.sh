8be2c381faeb0b082f27cfea846fee5dbc6ee16f
Loic Valery <lvalery@lxplus032.cern.ch>
2016-10-30 22:37:12 +0100

    new test for the w check

--- test/scripts/check_step_w.sh
+++ test/scripts/check_step_w.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-echo "=> Checking the w step by comparing the logfiles compared to expectations"
-diff -I "HistoAddress" -I "Opened input filo" -I "Pruning" LOG_w test/logs/LOG_w && diff FitExample/PruningText.txt test/FitExample/PruningText.txt && echo "toto"
-echo "=> Done checking the w step"
+diff -I "HistoAddress" -I "Opened input filo" -I "Pruning" LOG_w test/logs/LOG_w && diff FitExample/PruningText.txt test/FitExample/PruningText.txt
