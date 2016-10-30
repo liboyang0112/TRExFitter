682ff9f9cc37c9ac7408d364f1bfcc39f7aed05c
Loic Valery <lvalery@lxplus032.cern.ch>
2016-10-30 22:41:47 +0100

    adding final check for w logfile

--- test/scripts/check_step_w.sh
+++ test/scripts/check_step_w.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-diff -I "HistoAddress" -I "Opened input filo" -I "Pruning" LOG_w test/logs/LOG_w && diff FitExample/PruningText.txt test/FitExample/PruningText.txt
+diff -I "HistoAddress" -I "Opened input file" -I "Pruning" LOG_w test/logs/LOG_w && diff FitExample/PruningText.txt test/FitExample/PruningText.txt
