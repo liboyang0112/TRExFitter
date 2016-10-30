0d564729ef1f2d2f26a7901abab38ba87e7f4731
Loic Valery <lvalery@lxplus032.cern.ch>
2016-10-30 23:00:07 +0100

    Adding more checks

---
+++ test/scripts/check_step_l.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+diff -I "lvalery" -I "Real time" -I "mkdir" LOG_l test/logs/LOG_l
---
+++ test/scripts/check_step_s.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+diff -I "lvalery" -I "Real time" -I "mkdir" -I "RooRealVar" LOG_s test/logs/LOG_s
