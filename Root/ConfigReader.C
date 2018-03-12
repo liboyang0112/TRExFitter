d0657b786256a293f11d9aa43c6ba5d7c3377cf4
Tomas Dado <tomas.dado@cern.ch>
2018-03-12 18:12:32 +0100

    ported Command line options to new framework

--- Root/ConfigReader.C
+++ Root/ConfigReader.C
@@ -1,12 +1,16 @@
 #include "TtHFitter/ConfigReader.h"
+
 #include "TtHFitter/TtHFit.h"
+#include "TtHFitter/StatusLogbook.h"
 
 
 ConfigReader::ConfigReader(TtHFit *fitter){
     fFitter = fitter;
+    WriteInfoStatus("ConfigReader::ConfigReader", "Started reading the config");
 }
 
 ConfigReader::~ConfigReader(){
+    WriteInfoStatus("ConfigReader::~ConfigReader", "Finished with the config reading");
 }
 
 
@@ -28,26 +32,104 @@ int ConfigReader::ReadFullConfig(const std::string& fileName, const std::string&
     //
     // Read options (to skip stuff, or include only some regions, samples, systs...)
     // Syntax: .. .. Regions=ge4jge2b:Exclude=singleTop,wjets
-    std::map< std::string,std::string > optMap;
-    std::vector< std::string > optVec;
-    std::vector< std::string > onlyRegions;
-    std::vector< std::string > onlySamples;
-    std::vector< std::string > onlySystematics;
-    std::vector< std::string > toExclude;
-    std::string onlySignal = "";*/
+    // */
 
     // read different types of settings
     if (option != ""){
-        sc+= ReadCommandLineOptions();
+        sc+= ReadCommandLineOptions(option);
     }
 
     sc+= ReadJobOptions();
 
-
     return 0;
 }
 
-int ConfigReader::ReadCommandLineOptions(){
+int ConfigReader::ReadCommandLineOptions(std::string option){
+    std::vector< std::string > optVec = Vectorize(option,':');
+    std::map< std::string,std::string > optMap;
+    
+    for(const std::string& iopt : optVec){
+        std::vector< std::string > optPair;
+        optPair = Vectorize(iopt,'=');
+        optMap[optPair[0]] = optPair[1];
+    }
+    if(optMap["Regions"]!=""){
+        fOnlyRegions = Vectorize(optMap["Regions"],',');
+    }
+    if(optMap["Samples"]!=""){
+        fOnlySamples = Vectorize(optMap["Samples"],',');
+    }
+    if(optMap["Systematics"]!=""){
+        fOnlySystematics = Vectorize(optMap["Systematics"],',');
+    }
+    if(optMap["Exclude"]!=""){
+        fToExclude = Vectorize(optMap["Exclude"],',');
+    }
+    if(optMap["Suffix"]!=""){
+        fFitter->fSuffix = optMap["Suffix"]; // used for input & output  plots, txt files & workspaces - NOT for histograms file
+    }
+    if(optMap["SaveSuffix"]!=""){
+        fFitter->fSaveSuffix = optMap["SaveSuffix"]; // ... and this one for histograms file
+    }
+    if(optMap["Update"]!="" && optMap["Update"]!="FALSE"){
+        fFitter->fUpdate = true;
+    }
+    if(optMap["StatOnly"]!="" && optMap["StatOnly"]!="FALSE"){
+        fFitter->fStatOnly = true;
+    }
+    if(optMap["StatOnlyFit"]!="" && optMap["StatOnlyFit"]!="FALSE"){
+        fFitter->fStatOnlyFit = true;
+    }
+    if(optMap["Ranking"]!=""){
+        fFitter->fRankingOnly = optMap["Ranking"];
+    }
+    if(optMap["Signal"]!=""){
+        fOnlySignal = optMap["Signal"];
+    }
+    if(optMap["FitResults"]!=""){
+        fFitter->fFitResultsFile = optMap["FitResults"];
+    }
+    if(optMap["FitType"]!=""){
+        if(optMap["FitType"]=="SPLUSB") fFitter->SetFitType(TtHFit::SPLUSB);
+        if(optMap["FitType"]=="BONLY")  fFitter->SetFitType(TtHFit::BONLY);
+    }
+    if(optMap["LumiScale"]!=""){
+        fFitter->fLumiScale = atof(optMap["LumiScale"].c_str());
+    }
+    if(optMap["BootstrapIdx"]!=""){
+        fFitter->fBootstrapIdx = atoi(optMap["BootstrapIdx"].c_str());
+    }
+    //
+    WriteInfoStatus("ConfigReader::ReadCommandLineOptions", "-------------------------------------------");
+    WriteInfoStatus("ConfigReader::ReadCommandLineOptions", "Running options: ");
+    if(fOnlyRegions.size()>0){
+        WriteInfoStatus("ConfigReader::ReadCommandLineOptions", "  Only these Regions: ");
+        for(const std::string& ireg : fOnlyRegions){
+            WriteInfoStatus("ConfigReader::ReadCommandLineOptions", "    " + ireg);
+        }
+    }
+    if(fOnlySamples.size()>0){
+        WriteInfoStatus("ConfigReader::ReadCommandLineOptions", "  Only these Samples: ");
+        for(const std::string& isamp : fOnlySamples){
+            WriteInfoStatus("ConfigReader::ReadCommandLineOptions", "    " + isamp);
+        }
+    }
+    if(fOnlySystematics.size()>0){
+        WriteInfoStatus("ConfigReader::ReadCommandLineOptions", "  Only these Systematics: ");
+        for(const std::string& isyst : fOnlySystematics){
+            WriteInfoStatus("ConfigReader::ReadCommandLineOptions", "    " + isyst);
+        }
+    }
+    if(fToExclude.size()>0){
+        WriteInfoStatus("ConfigReader::ReadCommandLineOptions", "  Exclude: ");
+        for(const std::string& iexcl : fToExclude){
+            WriteInfoStatus("ConfigReader::ReadCommandLineOptions", "    " + iexcl);
+        }
+    }
+    if(fOnlySignal!=""){
+        WriteInfoStatus("ConfigReader::ReadCommandLineOptions", "  Only Signal: ");
+        WriteInfoStatus("ConfigReader::ReadCommandLineOptions", "    " + fOnlySignal);
+    }
     return 0;
 }
 
--- TtHFitter/ConfigReader.h
+++ TtHFitter/ConfigReader.h
@@ -1,33 +1,97 @@
 #ifndef CONFIGREADER_H
 #define CONFIGREADER_H
 
+// TtHFitter class includes
 #include "TtHFitter/ConfigParser.h"
 
+// standard c++ includes
 #include <string>
 
+//Forward class declaration
 class TtHFit;
+
+/**
+ * \class ConfigReader
+ * \brief Class that reads config parameters
+ * \author Tomas Dado
+ */
 
 class ConfigReader {
     public:
-       ConfigReader(TtHFit * fitter);
 
-       ~ConfigReader();
+        /**
+          * The default constructor.
+          * @param fitter A pointer to TtHFit class
+          */
+        ConfigReader(TtHFit * fitter);
 
-       int ReadFullConfig(const std::string& fileName, const std::string& option);
+        /**
+          * The default destructor
+          */ 
+        ~ConfigReader();
+
+        /**
+          * Reads the config and passes parameters to TtHFit
+          * @param string Config path 
+          * @param string Additional options
+          * @return int status code
+          */ 
+        int ReadFullConfig(const std::string& fileName, const std::string& option);
 
     private:
 
-        // private methods:
-        int ReadCommandLineOptions();
+        /**
+          * Helped function to read Command line settings
+          * @param string config options
+          * @return int status code
+          */
+        int ReadCommandLineOptions(std::string option);
         
+        /**
+          * Helped function to read JOB settings
+          * @return int status code
+          */
         int ReadJobOptions();
 
-        // private members
+        /**
+          * Pointer to TtHFit class, set during initialization
+          */
         TtHFit *fFitter;
     
+        /**
+          * Instance of ConfigSet needed to read the config
+          */ 
         ConfigSet fConfSet;
 
+        /**
+          * Instance of ConfigParser used to parse the text
+          */
         ConfigParser fParser;
+   
+        /**
+          * vector of strings, one for each region
+          */ 
+        std::vector< std::string > fOnlyRegions;
+        
+        /**
+          * vector of strings, one for each sample
+          */ 
+        std::vector< std::string > fOnlySamples;
+        
+        /**
+          * vector of strings, one for each systematics
+          */ 
+        std::vector< std::string > fOnlySystematics;
+        
+        /**
+          * vector of strings, one for each exclude region
+          */ 
+        std::vector< std::string > fToExclude;
+        
+        /**
+          *  string for signal only
+          */ 
+        std::string fOnlySignal = "";
 };
 
 #endif
