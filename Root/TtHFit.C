dfb6d8a728fbfd82fd2e6b61206640d2c1427b53
Michele Pinamonti <michele.pinamonti@gmail.com>
2018-03-21 14:04:00 +0000

    fixing conflicts for dc10c6836dc707bcbbcc6194d43e8744a8c1ce98

--- Root/TtHFit.C
+++ Root/TtHFit.C
@@ -7411,32 +7411,33 @@ const bool TtHFit::MorphIsAlreadyPresent(const std::string& name, const float va
 //____________________________________________________________________________________
 // create a map associating parameters to their SubCategory
 void TtHFit::ProduceSystSubCategoryMap(){
-   WriteDebugStatus("TtHFit::ProduceSystSubCategoryMap", "filling SubCategory map");
+    WriteDebugStatus("TtHFit::ProduceSystSubCategoryMap", "filling SubCategory map");
+    
+    // special treatment needed for two cases:
+    // 1) stat-only fit where all parameters are fixed, see FittingTool::GetGroupedImpact()
+    // 2) fit with all Gammas fixed, see FittingTool::GetGroupedImpact()
+    fSubCategoryImpactMap.insert(std::make_pair("DUMMY_STATONLY", "FullSyst"));
+    fSubCategoryImpactMap.insert(std::make_pair("DUMMY_GAMMAS", "Gammas"));
+    
+    // add all systematics, here an "alpha_" prefix is needed
+    for(int i_syst=0;i_syst<fNSyst;i_syst++){
+        //std::cout << fSystematics[i_syst]->fName << endl;
+    if(fSystematics[i_syst]->fSubCategory=="Gammas" or fSystematics[i_syst]->fSubCategory=="FullSyst" or fSystematics[i_syst]->fSubCategory=="combine")
+        WriteWarningStatus("TtHFit::ProduceSystSubCategoryMap"," use of \"Gammas\", \"FullSyst\" or \"combine\" as SubCategory names is not supported, you will likely run into issues");
+//        fSubCategoryImpactMap.insert(std::make_pair(("alpha_" + fSystematics[i_syst]->fName).c_str(), fSystematics[i_syst]->fSubCategory));
+        fSubCategoryImpactMap.insert(std::make_pair(("alpha_" + fSystematics[i_syst]->fNuisanceParameter).c_str(), fSystematics[i_syst]->fSubCategory));
+    }
 
-   // special treatment needed for two cases:
-   // 1) stat-only fit where all parameters are fixed, see FittingTool::GetGroupedImpact()
-   // 2) fit with all Gammas fixed, see FittingTool::GetGroupedImpact()
-   fSubCategoryImpactMap.insert(std::make_pair("DUMMY_STATONLY", "FullSyst"));
-   fSubCategoryImpactMap.insert(std::make_pair("DUMMY_GAMMAS", "Gammas"));
-
-   // add all systematics, here an "alpha_" prefix is needed
-   for(int i_syst=0;i_syst<fNSyst;i_syst++){
-       //std::cout << fSystematics[i_syst]->fName << endl;
-       if(fSystematics[i_syst]->fSubCategory=="Gammas" or fSystematics[i_syst]->fSubCategory=="FullSyst" or fSystematics[i_syst]->fSubCategory=="combine")
-           WriteWarningStatus("TtHFit::ProduceSystSubCategoryMap"," use of \"Gammas\", \"FullSyst\" or \"combine\" as SubCategory names is not supported, you will likely run into issues");
-       fSubCategoryImpactMap.insert(std::make_pair(("alpha_" + fSystematics[i_syst]->fName).c_str(), fSystematics[i_syst]->fSubCategory));
-   }
-
-   // also add norm factors, no "alpha_" needed
-   for(int i_nf=0;i_nf<fNNorm;i_nf++){
-       //std::cout << fNormFactors[i_nf]->fName << endl;
-       if(fNormFactors[i_nf]->fSubCategory=="Gammas" or fNormFactors[i_nf]->fSubCategory=="FullSyst" or fNormFactors[i_nf]->fSubCategory=="combine")
-           WriteWarningStatus("TtHFit::ProduceSystSubCategoryMap"," use of \"Gammas\", \"FullSyst\" or \"combine\" as SubCategory names is not supported, you will likely run into issues");
-       if (fNormFactors[i_nf]->fName != fPOI) {
-//            fSubCategoryImpactMap.insert(std::make_pair(fNormFactors[i_nf]->fName, fNormFactors[i_nf]->fSubCategory));
-           fSubCategoryImpactMap.insert(std::make_pair(fNormFactors[i_nf]->fNuisanceParameter, fNormFactors[i_nf]->fSubCategory));
-       }
-   }
+    // also add norm factors, no "alpha_" needed
+    for(int i_nf=0;i_nf<fNNorm;i_nf++){
+        //std::cout << fNormFactors[i_nf]->fName << endl;
+        if(fNormFactors[i_nf]->fSubCategory=="Gammas" or fNormFactors[i_nf]->fSubCategory=="FullSyst" or fNormFactors[i_nf]->fSubCategory=="combine")
+            WriteWarningStatus("TtHFit::ProduceSystSubCategoryMap"," use of \"Gammas\", \"FullSyst\" or \"combine\" as SubCategory names is not supported, you will likely run into issues");
+        if (fNormFactors[i_nf]->fName != fPOI) {
+//             fSubCategoryImpactMap.insert(std::make_pair(fNormFactors[i_nf]->fName, fNormFactors[i_nf]->fSubCategory));
+            fSubCategoryImpactMap.insert(std::make_pair(fNormFactors[i_nf]->fNuisanceParameter, fNormFactors[i_nf]->fSubCategory));
+        }
+    }
 }
 
 //____________________________________________________________________________________
