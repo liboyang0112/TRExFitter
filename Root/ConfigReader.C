a0086f1186276be2fd50a7b9da20dd2cbb2897fd
Tomas Dado <tomas.dado@cern.ch>
2018-08-23 13:19:01 +0200

    Now works with wildcards

--- Root/ConfigReader.C
+++ Root/ConfigReader.C
@@ -3705,7 +3705,9 @@ bool ConfigReader::CheckPresence(const std::vector<std::string> &v1, const std::
         if (i == "") continue;
         if (i == "none") continue;
         if (i == "all") continue;
-        if (std::find(v2.begin(), v2.end(), i) == v2.end()) return false;
+        if (FindInStringVector(v2, i) < 0){
+            return false;
+        }
     }
 
     return true;
@@ -3718,8 +3720,10 @@ bool ConfigReader::CheckPresence(const std::vector<std::string> &v1, const std::
         if (i == "") continue;
         if (i == "none") continue;
         if (i == "all") continue;
-        if (std::find(v2.begin(), v2.end(), i) == v2.end()){
-            if (std::find(v3.begin(), v3.end(), i) == v3.end()) return false;
+        if (FindInStringVector(v2, i) < 0){
+            if (FindInStringVector(v3, i) < 0){
+                return false;
+            }
         }
     }
 
