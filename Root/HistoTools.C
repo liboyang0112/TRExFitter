5b64beb774b059abb61b23816ddd9685fc5d27fa
Tomas Dado <tomas.dado@cern.ch>
2018-08-24 16:09:42 +0200

    use relative uncertainty

--- Root/HistoTools.C
+++ Root/HistoTools.C
@@ -279,31 +279,6 @@ float HistoTools::Separation(TH1* h1,TH1* h2){
 //
 TH1D* HistoTools::SymmetrizeTwoSided(TH1* var1, TH1* var2, TH1* hnom) {
 
-    // check if the variations are not in the same direction
-    for (int ibin = 1; ibin <= hnom->GetNbinsX(); ibin++){
-        const double& nom_content = hnom->GetBinContent(ibin);
-        const double& nom_error = hnom->GetBinError(ibin);
-        const double& up_content = var1->GetBinContent(ibin);
-        const double& up_error = var1->GetBinError(ibin);
-        const double& down_content = var2->GetBinContent(ibin);
-        const double& down_error = var2->GetBinError(ibin);
-
-        const double total_uncert_up = std::sqrt(up_error*up_error + nom_error*nom_error);
-        const double total_uncert_down = std::sqrt(down_error * down_error + nom_error*nom_error);
-
-        // check if the variations are larger than stat uncertainty
-        // have same sign = same shift
-        if ((std::fabs(up_content - nom_content) > total_uncert_up) && (std::fabs(down_content - nom_content) > total_uncert_down)){
-            // check if the variations have the same shift
-            if ((up_content - nom_content) * (down_content - nom_content) > 0){
-                const std::string& name_up = var1->GetName();
-                const std::string& name_down = var2->GetName();
-                WriteWarningStatus("HistoTools::SymmetrizeTwoSided", "Histograms " + name_up + " and " + name_down + " in bin " + std::to_string(ibin) + " have the same shift for up and down variation");
-                WriteWarningStatus("HistoTools::SymmetrizeTwoSided", "You should check this");
-            }
-        }
-    }
-
     //
     // Symmetrize a variation that is already two sided to smooth out possible fluctuations
     //
@@ -319,6 +294,31 @@ TH1D* HistoTools::SymmetrizeTwoSided(TH1* var1, TH1* var2, TH1* hnom) {
     TH1D* tmp2 = (TH1D* )var2->Clone();
     tmp2->Divide(nom);
     if(!tmp2->GetSumw2())tmp2->Sumw2();
+
+    // check if the variations are not in the same direction
+    for (int ibin = 1; ibin <= hnom->GetNbinsX(); ibin++){
+        const double& nom_error = hnom->GetBinError(ibin);
+        const double& up_content = tmp1->GetBinContent(ibin);
+        const double& up_error = tmp1->GetBinError(ibin);
+        const double& down_content = tmp2->GetBinContent(ibin);
+        const double& down_error = tmp2->GetBinError(ibin);
+
+        const double total_uncert_up = std::sqrt(up_error*up_error + nom_error*nom_error);
+        const double total_uncert_down = std::sqrt(down_error*down_error + nom_error*nom_error);
+
+        // check if the variations are larger than stat uncertainty
+        if ((std::fabs(up_content - 1) > total_uncert_up) && (std::fabs(down_content - 1) > total_uncert_down)){
+            // check if the variations have the same shift
+            // have same sign = same shift
+            if ((up_content - 1) * (down_content - 1) > 0){
+                const std::string& name_up = var1->GetName();
+                const std::string& name_down = var2->GetName();
+                WriteWarningStatus("HistoTools::SymmetrizeTwoSided", "Histograms " + name_up + " and " + name_down + " in bin " + std::to_string(ibin) + " have the same shift for up and down variation");
+                WriteWarningStatus("HistoTools::SymmetrizeTwoSided", "You should check this");
+            }
+        }
+    }
+
 
     //Flat (content = 0) histogram to substract
     TH1D* unit = (TH1D* )nom->Clone();
