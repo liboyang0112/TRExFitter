95370103633b9cf60fc85415d4e7dd2ff6eb63d4
Tomas Dado <tomas.dado@cern.ch>
2019-02-21 21:54:27 +0100

    Allocating on stack - CorrelationMatrix

--- Root/CorrelationMatrix.C
+++ Root/CorrelationMatrix.C
@@ -121,30 +121,30 @@ void CorrelationMatrix::Draw(const std::string& path, const bool& useGammas, con
     //
     // 1) Performs the plot
     //
-    TH2D *h_corr = new TH2D("h_corr","",N,0,N,N,0,N);
-    h_corr->SetDirectory(0);
+    TH2D h_corr("h_corr","",N,0,N,N,0,N);
+    h_corr.SetDirectory(0);
 
     for(unsigned int iNP = 0; iNP < vec_NP.size(); ++iNP){//line number
         const string iSystName = vec_NP[iNP];
 
         if(TRExFitter::SYSTMAP[iSystName]!=""){
-            h_corr->GetXaxis()->SetBinLabel(iNP+1,(TString)TRExFitter::SYSTMAP[iSystName]);
-            h_corr->GetYaxis()->SetBinLabel(N-iNP,(TString)TRExFitter::SYSTMAP[iSystName]);
+            h_corr.GetXaxis()->SetBinLabel(iNP+1,(TString)TRExFitter::SYSTMAP[iSystName]);
+            h_corr.GetYaxis()->SetBinLabel(N-iNP,(TString)TRExFitter::SYSTMAP[iSystName]);
         }
         else{
-            h_corr->GetXaxis()->SetBinLabel(iNP+1,(TString)iSystName);
-            h_corr->GetYaxis()->SetBinLabel(N-iNP,(TString)iSystName);
+            h_corr.GetXaxis()->SetBinLabel(iNP+1,(TString)iSystName);
+            h_corr.GetYaxis()->SetBinLabel(N-iNP,(TString)iSystName);
         }
 
         for(unsigned int jNP = 0; jNP < vec_NP.size(); ++jNP){//column number
             const string jSystName = vec_NP[jNP];
 
-            h_corr -> SetBinContent(iNP+1,N-jNP,100.*GetCorrelation(iSystName, jSystName));
+            h_corr.SetBinContent(iNP+1,N-jNP,100.*GetCorrelation(iSystName, jSystName));
 
         }
     }
-    h_corr->SetMinimum(-100.);
-    h_corr->SetMaximum(100.);
+    h_corr.SetMinimum(-100.);
+    h_corr.SetMaximum(100.);
 
     int size = 500;
     if(vec_NP.size()>10){
@@ -154,26 +154,25 @@ void CorrelationMatrix::Draw(const std::string& path, const bool& useGammas, con
     //
     // 2) Style settings
     //
-    TCanvas *c1 = new TCanvas("","",0.,0.,size+300,size+300);
+    TCanvas c1("","",0.,0.,size+300,size+300);
     gStyle->SetPalette(1);
-    h_corr->SetMarkerSize(0.75*1000);
+    h_corr.SetMarkerSize(0.75*1000);
     gStyle->SetPaintTextFormat(".1f");
     gPad->SetLeftMargin(0.5*600/(size+300));
     gPad->SetBottomMargin(0.5*600/(size+300));
     gPad->SetRightMargin(0.);
     gPad->SetTopMargin(0.);
 
-    h_corr->GetXaxis()->LabelsOption("v");
-    h_corr->GetXaxis()->SetLabelSize( h_corr->GetXaxis()->GetLabelSize()*0.75 );
-    h_corr->GetYaxis()->SetLabelSize( h_corr->GetYaxis()->GetLabelSize()*0.75 );
-    c1->SetTickx(0);
-    c1->SetTicky(0);
-    h_corr->GetYaxis()->SetTickLength(0);
-    h_corr->GetXaxis()->SetTickLength(0);
-    c1->SetGrid();
-    h_corr->Draw("col TEXT");
-    c1->RedrawAxis("g");
+    h_corr.GetXaxis()->LabelsOption("v");
+    h_corr.GetXaxis()->SetLabelSize( h_corr.GetXaxis()->GetLabelSize()*0.75 );
+    h_corr.GetYaxis()->SetLabelSize( h_corr.GetYaxis()->GetLabelSize()*0.75 );
+    c1.SetTickx(0);
+    c1.SetTicky(0);
+    h_corr.GetYaxis()->SetTickLength(0);
+    h_corr.GetXaxis()->SetTickLength(0);
+    c1.SetGrid();
+    h_corr.Draw("col TEXT");
+    c1.RedrawAxis("g");
 
-    c1->SaveAs(path.c_str());
-    delete c1;
+    c1.SaveAs(path.c_str());
 }
