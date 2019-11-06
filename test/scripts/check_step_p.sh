109511491b1642c2cb63534fa1720d08bf4af83a
Tomas Dado <tomas.dado@cern.ch>
2019-11-06 14:37:48 +0100

    Fixing d and p step

--- test/scripts/check_step_p.sh
+++ test/scripts/check_step_p.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-diff -w LOG_p test/logs/LOG_p && for file in `ls FitExample/Tables/*postFit.txt`; do diff -w $file test/$file; done
+diff -w -I "libSM.so" -I "libASImage" -I "png file FitExample" LOG_p test/logs/LOG_p && for file in `ls FitExample/Tables/*postFit.txt`; do diff -w $file test/$file; done
