fcbb6a61c5c4ab21e045b9bbaad0a22154725cde
Tomas Dado <tomas.dado@cern.ch>
2019-11-13 21:19:32 +0100

    Updating setup script to use 4 cores for compilation

--- setup.sh
+++ setup.sh
@@ -74,9 +74,9 @@ echo "Setting up cmake with: lsetup cmake"
 lsetup cmake
 
 export PATH=${PATH}${PATH:+:}${TREXFITTER_HOME}/build/bin
-alias trex-make='cd build/; cmake --build ./; cd ../'
+alias trex-make='cd build/; make -j4; cd ../'
 alias trex-clean='rm -rf build/'
-alias trex-build='mkdir build && cd build; cmake ../; cmake --build ./; cd ../'
+alias trex-build='mkdir build && cd build; cmake ../; make -j4; cd ../'
 
 version=`cat ${TREXFITTER_HOME}/version.txt`
 echo -e "\n\e[1m${version} ready (if everything went smoothly)\e[0m"
