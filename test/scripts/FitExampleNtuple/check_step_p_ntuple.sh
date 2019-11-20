490396e58472eec1cb1a8274e889044af80c92be
Alexander Held <alexander.held@cern.ch>
2019-11-20 14:22:04 -0800

    fixing paths in CI check scripst

--- test/scripts/FitExampleNtuple/check_step_p_ntuple.sh
+++ test/scripts/FitExampleNtuple/check_step_p_ntuple.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-diff -w -I "libSM.so" -I "libASImage" -I "png file FitExampleNtuple" LOG_NTUPLE_p test/logs/LOG_NTUPLE_p && for file in `ls FitExampleNtuple/Tables/*postFit.txt`; do diff -w $file test/$file; done
+diff -w -I "libSM.so" -I "libASImage" -I "png file FitExampleNtuple" LOG_NTUPLE_p test/logs/FitExampleNtuple/LOG_NTUPLE_p && for file in `ls FitExampleNtuple/Tables/*postFit.txt`; do diff -w $file test/$file; done
