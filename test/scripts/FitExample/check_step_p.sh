188d27ac75a928ae8ad7edb208ff506b4f1a7e34
Tomas Dado <tomas.dado@cern.ch>
2019-11-21 10:12:24 +0100

    Fixing CI

--- test/scripts/FitExample/check_step_p.sh
+++ test/scripts/FitExample/check_step_p.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-diff -w -I "libSM.so" -I "libASImage" -I "png file FitExample" LOG_p test/logs/FitExample/LOG_p && for file in `ls FitExample/Tables/*postFit.txt`; do diff -w $file test/reference/FitExample/$file; done
+diff -w -I "libSM.so" -I "libASImage" -I "png file FitExample" LOG_p test/logs/FitExample/LOG_p && for file in `ls FitExample/Tables/*postFit.txt`; do diff -w $file test/reference/$file; done
