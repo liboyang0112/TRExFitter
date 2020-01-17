c27a83a548cdc3a7b3a8da99a40ec33e5be25cc8
Tomas Dado <tomas.dado@cern.ch>
2020-01-17 11:53:22 +0100

    Fixing error when normalizing

--- UnfoldingCode/Root/UnfoldingTools.cc
+++ UnfoldingCode/Root/UnfoldingTools.cc
@@ -18,7 +18,9 @@ void UnfoldingTools::NormalizeMatrix(TH2* matrix, const bool byRow) {
             if (sum < 1e-9) continue;
             for (int ibinx = 1; ibinx <= matrix->GetNbinsX(); ++ibinx) {
                 const double content = matrix->GetBinContent(ibinx, ibiny);
+                const double error = matrix->GetBinError(ibinx, ibiny);
                 matrix->SetBinContent(ibinx, ibiny, content/sum);
+                matrix->SetBinError  (ibinx, ibiny, error*(content/sum));
             }
         }
     } else {
@@ -30,7 +32,9 @@ void UnfoldingTools::NormalizeMatrix(TH2* matrix, const bool byRow) {
             if (sum < 1e-9) continue;
             for (int ibiny = 1; ibiny <= matrix->GetNbinsY(); ++ibiny) {
                 const double content = matrix->GetBinContent(ibinx, ibiny);
+                const double error = matrix->GetBinError(ibinx, ibiny);
                 matrix->SetBinContent(ibinx, ibiny, content/sum);
+                matrix->SetBinError  (ibinx, ibiny, error*(content/sum));
             }
         }
     }
