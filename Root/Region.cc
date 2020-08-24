b9c0cb1ccf3393ca983f5f6f1a87a541e717932f
Alexander Held <alexander.held@cern.ch>
2020-08-24 14:13:44 +0000

    perf: use symmetry of correlation matrix in uncertainty band calculation

--- Root/Region.cc
+++ Root/Region.cc
@@ -2458,41 +2458,42 @@ std::unique_ptr<TGraphAsymmErrors> BuildTotError( const TH1* const h_nominal,
     for(int i_bin=1; i_bin < h_nominal->GetNbinsX()+1; ++i_bin){
         double finalErrPlus(0.);
         double finalErrMinus(0.);
-        double corr(0.);
         // yieldNominal = h_nominal->GetBinContent(i_bin);
         // - loop on the syst, two by two, to include the correlations
-        for(std::size_t i_syst=0; i_syst < EffectiveSystNames.size(); ++i_syst){
-            for(std::size_t j_syst=0; j_syst < EffectiveSystNames.size(); ++j_syst){
-                if (i_syst==j_syst) continue;
-                if(matrix!=nullptr){
+        if (matrix!=nullptr) {
+            double corr(0.);
+            // if the correlation matrix is not provided, there are no off-diagonal contributions
+            // to include, so these loops can be skipped
+            for(std::size_t i_syst=0; i_syst < EffectiveSystNames.size(); ++i_syst){
+                for(std::size_t j_syst=0; j_syst < i_syst; ++j_syst){
+                    // the inner loop only runs up to i_syst-1, effectively going over half
+                    // the correlation matrix (the other half is symmetric)
                     corr = matrix->GetCorrelation(EffectiveSystNames[i_syst],EffectiveSystNames[j_syst]);
-                }
-                else{
-                    if(EffectiveSystNames[i_syst]==EffectiveSystNames[j_syst]) corr = 1.;
-                    else                                                       corr = 0.;
-                }
-                const double errUp_i   = h_up[EffectiveSystIndex[i_syst]]  ->GetBinContent(i_bin);// - yieldNominal;
-                const double errDown_i = h_down[EffectiveSystIndex[i_syst]]->GetBinContent(i_bin);// - yieldNominal;
-                const double errUp_j   = h_up[EffectiveSystIndex[j_syst]]  ->GetBinContent(i_bin);// - yieldNominal;
-                const double errDown_j = h_down[EffectiveSystIndex[j_syst]]->GetBinContent(i_bin);// - yieldNominal;
 
-                //
-                // Symmetrize (seems to be done in Roostats ??)
-                //
-                const double err_i = (errUp_i - errDown_i)/2.;
-                const double err_j = (errUp_j - errDown_j)/2.;
+                    const double errUp_i   = h_up[EffectiveSystIndex[i_syst]]  ->GetBinContent(i_bin);
+                    const double errDown_i = h_down[EffectiveSystIndex[i_syst]]->GetBinContent(i_bin);
+                    const double errUp_j   = h_up[EffectiveSystIndex[j_syst]]  ->GetBinContent(i_bin);
+                    const double errDown_j = h_down[EffectiveSystIndex[j_syst]]->GetBinContent(i_bin);
 
-                //
-                // Compute the + and - variations
-                //
-                finalErrPlus  += err_i * err_j * corr;
-                finalErrMinus += err_i * err_j * corr;
+                    //
+                    // Symmetrize (seems to be done in Roostats ??)
+                    //
+                    const double err_i = (errUp_i - errDown_i)/2.;
+                    const double err_j = (errUp_j - errDown_j)/2.;
+
+                    //
+                    // Compute the + and - variations
+                    //
+                    // needs a factor 2 at the end since we are looping over half the correlation matrix
+                    finalErrPlus  += err_i * err_j * corr * 2;
+                    finalErrMinus += err_i * err_j * corr * 2;
+                }
             }
         }
         // now all diagonal el. of all systematics, corr = 1;
         for(std::size_t i_syst=0; i_syst<fSystNames.size(); ++i_syst){
-            const double errUp_i   = h_up[i_syst]  ->GetBinContent(i_bin);// - yieldNominal;
-            const double errDown_i = h_down[i_syst]->GetBinContent(i_bin);// - yieldNominal;
+            const double errUp_i   = h_up[i_syst]  ->GetBinContent(i_bin);
+            const double errDown_i = h_down[i_syst]->GetBinContent(i_bin);
 
             //
             // Symmetrize (seems to be done in Roostats ??)
